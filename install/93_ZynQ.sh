2882384f1566fdbdc883f128a5f9d04078c93ed8
Tiago Jorge <tmdasilva@gmv.com>
2019-11-27 13:26:12 +0000

    Add status comments. Add README to Vivado project dir.

--- install/93_ZynQ.sh
+++ install/93_ZynQ.sh
@@ -4,6 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${DIR} || exit 1
 
+# TODO build any needed libraries (e.g. AXI) and copy them under 'share'
+
 grep ZYNQZC706 $HOME/.bashrc.taste >/dev/null || {
     echo 'export ZYNQZC706=$HOME/tool-src/misc/ZynQzc706/TASTE-VHDL-DESIGN.tar.bz2' >> $HOME/.bashrc.taste
 }
