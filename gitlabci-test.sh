b39c0d07c7f007b4bb67078f8cdeb420d168fada
Maxime Perrotin <Maxime.Perrotin@esa.int>
2020-05-13 14:11:19 +0200

    Try something else

--- gitlabci-test.sh
+++ gitlabci-test.sh
@@ -6,7 +6,10 @@ export TASTE_IN_DOCKER=1
 git fetch
 git checkout -f "${CI_COMMIT_BRANCH}" 
 ./Update-TASTE.sh
-source ~/.bashrc.taste
+cd kazoo 
+export PATH=$HOME/tool-inst/bin:$PATH
+make test || exit 1
+cd ..
 cd dmt
 git log | head
 /etc/init.d/postgresql start 
@@ -14,9 +17,6 @@ git log | head
 pip3 install --upgrade .
 pip3 uninstall --yes typing 
 PATH=$PATH:/asn1scc/ LANG=C LC_ALL=C make
-cd ..
-cd kazoo 
-make test || exit 1
 cd ..
 cd opengeode
 make test-ada || exit 1
