528541895dc85cacfac18c960a44228e44d01ebf
Maxime Perrotin <Maxime.Perrotin@esa.int>
2020-05-22 18:02:02 +0200

    Test saving msc with asn1 file, in streaming mode

--- misc/helper-scripts/msc_viking_stream_python3.py
+++ misc/helper-scripts/msc_viking_stream_python3.py
@@ -25,7 +25,11 @@ if not path_to_msceditor:
 editor = subprocess.Popen ([path_to_msceditor, "-p", "5116"])
 
 # Prepare a command to the MSC Editor
-cmd = '{"CommandType": "Instance", "Parameters": {"name": "InstanceA"}}'
+cmds = ['{"CommandType": "Instance", "Parameters": {"name": "InstanceA"}}',
+       '{"CommandType": "Instance", "Parameters": {"name": "InstanceB"}}',
+       '{"CommandType": "Condition","Parameters": {"name": "StateBlah", "instanceName": "InstanceA", "shared": "true"}}',
+        '{"CommandType": "Action", "Parameters": {"name": "Welcome to TASTE", "instanceName": "InstanceA"}}',
+        '{"CommandType": "Save", "Parameters": {"fileName": "helloWorld.msc", "asn1File": "DataView.asn"}}']
 
 # These are the possible commands:
 #{"CommandType": "VisibleItemLimit", "Parameters": {"number": "10"}}
@@ -33,11 +37,13 @@ cmd = '{"CommandType": "Instance", "Parameters": {"name": "InstanceA"}}'
 #{"CommandType": "Message", "Parameters": {"name": "Message_A-B","srcName": "Instance_A","dstName": "Instance_B"}}
 #{"CommandType": "Timer", "Parameters": {"name": "Timer_A_1","instanceName": "Instance_A","TimerType": "Start"}}
 # TimerType can then be "Stop" and "Timeout"
-#{"CommandType": "Condition","Parameters": {"name": "Condition_1", "instanceName": "Instance_B"}}
+#{"CommandType": "Condition","Parameters": {"name": "Condition_1", "instanceName": "Instance_B", "shared": "true"}}
+#{"CommandType": "Action", "Parameters": {"name": "SomeAction", "instanceName": "InstanceA"}}
 #{"CommandType": "Message","Parameters": {"name": "Message_C-dynamic", "srcName": "Instance_C","dstName": "Instance_dynamic", "MessageType": "Create"}}
 #{"CommandType": "StopInstance", "Parameters": {"name": "Instance_dynamic_2"}}
 #{"CommandType": "Undo"}
 #{"CommandType": "Redo"}
+#{"CommandType": "Save", "Parameters": {"fileName": "helloWorld.msc", "asn1File": "dataview-uniq.asn"}}
 
 # Give some time for the MSC Editor process to open the socket
 time.sleep(0.5)
@@ -45,8 +51,9 @@ time.sleep(0.5)
 # Establish a connection with the MSC Editor
 ws = create_connection ("ws://localhost:5116/")
 
-# Send the command
-ws.send(cmd)
+# Send the commands
+for cmd in cmds:
+    ws.send(cmd)
 
 input ("Press enter to close")
 
