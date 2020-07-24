eafa42206e957aeb7290a2747622587db5531920
Thanassis Tsiodras <ttsiodras@gmail.com>
2020-07-24 18:40:08 +0200

    Remove previous versions of spacecreator if already existing.

--- install/88_spaceCreator.sh
+++ install/88_spaceCreator.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 dpkg -l | grep '^ii.*spacecreator.*0.1.6d93e844' > /dev/null || {
-    echo "Installing the latest Space Creator..."
+    dpkg -l | grep spacecreator > /dev/null && {
+        echo "[-] Uninstalling previously existing version..."
+        sudo apt remove -y --force-yes spacecreator || exit 1
+    }
+    echo "[-] Installing the latest Space Creator..."
     NEW_DEB=/tmp/newSpaceCreator.$$.deb
     if wget -O $NEW_DEB "https://download.tuxfamily.org/taste/SpaceCreator.deb" ; then
         sudo gdebi -n -o=--no-install-recommends $NEW_DEB || {
-            echo Failed to install $NEW_DEB...
+            echo "[x] Failed to install $NEW_DEB..."
             ls -l $NEW_DEB
-            echo Aborting.
+            echo "[x] Aborting."
             exit 1
         }
         rm -f $NEW_DEB
     else
-        echo Failed to download the new Space Creator... Aborting.
+        echo "[x] Failed to download the new Space Creator... Aborting."
         exit 1
     fi
 }
