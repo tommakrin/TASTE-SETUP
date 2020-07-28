a1740d6b99cfe453a8e7f304c15da44effef6ffe
Maxime Perrotin <Maxime.Perrotin@esa.int>
2020-07-28 15:21:25 +0200

    don't prompt user when package is installed

--- install/03_debian.sh
+++ install/03_debian.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 if [ "${ARCH}" == "x86_64" ] ; then
     sudo apt-get install --no-install-recommends -y --force-yes libc6:i386 libgcc1:i386 libxft2:i386 libxss1:i386 libcairo2:i386 libc6-dev-i386 
 fi
-sudo apt-get install --no-install-recommends lsb-release
+sudo apt-get install --no-install-recommends -y --force-yes lsb-release
 
 if [ -z "$VERSION" ] ; then
     VERSION="$(lsb_release -d)"
