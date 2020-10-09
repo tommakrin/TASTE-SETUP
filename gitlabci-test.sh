ca2778a8efc8dbbeed4b28b58af6e600c4389505
Thanassis Tsiodras <ttsiodras@gmail.com>
2020-10-09 10:50:42 +0200

    Quick test on updated gitlab runner.

--- gitlabci-test.sh
+++ gitlabci-test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Testing updated gitlab runner
+echo 100 > /proc/sys/fs/mqueue/msg_max
+cat /proc/sys/fs/mqueue/msg_max
+exit 1
 /etc/init.d/postgresql start 
 source ~/.bashrc
 set -e
