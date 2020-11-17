f02068b96f88a578f577e7e44dc7d803d0b51040
Thanassis Tsiodras <Thanassis.Tsiodras@esa.int>
2020-11-17 17:28:04 +0100

    Sync with upstream

--- install/54_speedometer.sh
+++ install/54_speedometer.sh
@@ -3,13 +3,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "${DIR}/common.sh"
 
 # Setup speedometer library in ~/.local
-cd "$DIR/../speedometer" || exit 1
+cd "$DIR/../speedometer/python3" || exit 1
 
 # Skip install if the version installed is the same and the tree is clean
 HEAD="$(grep version speedometer/speedometer.py  | head -1 | awk -F\" '{print $(NF-1)}')"
 # This is much slower than a --version option would be...
 # But there's no such functionality in speedometer :-(
-VERSION_INSTALLED="$(pip2 freeze | grep speedometer | awk -F= '{print $NF}')"
+VERSION_INSTALLED="$(pip3 freeze | grep speedometer | awk -F= '{print $NF}')"
 GIT_OUTPUT=$(git status --porcelain)
 if [ "${GIT_OUTPUT}" == "" ] ; then
     TREE_DIRTY=0
@@ -25,7 +25,8 @@ fi
 # Unfortunately, the --upgrade DOES NOT ALWAYS WORK.
 # Uninstall first...
 echo y | pip2 uninstall speedometer
-pip2 install --user --upgrade . || exit 1
+echo y | pip3 uninstall speedometer
+pip3 install --user --upgrade . || exit 1
 
 # Add .local/bin to PATH
 PATH_CMD='export PATH=$PATH:$HOME/.local/bin'
