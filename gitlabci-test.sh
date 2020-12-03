da00f778b04a71f4ff84de18a73f8783df330da9
Maxime Perrotin <Maxime.Perrotin@esa.int>
2020-12-03 18:05:56 +0100

    Bybass questions when installing add-ons

--- gitlabci-test.sh
+++ gitlabci-test.sh
@@ -7,10 +7,10 @@ export TASTE_IN_DOCKER=1
 git fetch
 git checkout -f "${CI_COMMIT_BRANCH}" 
 ./Update-TASTE.sh
-./add-ons/install-msp430-gcc.sh || exit 1
+echo y | ./add-ons/install-msp430-gcc.sh || exit 1
 ./add-ons/install-gnat2020-for-arm.sh  || exit 1
-./add-ons/install-adac-hybrid-msp430.sh || exit 1
-./add-ons/install-freertos.sh || exit 1
+echo y | ./add-ons/install-adac-hybrid-msp430.sh || exit 1
+echo y | ./add-ons/install-freertos.sh || exit 1
 source ~/.bashrc.taste
 cd kazoo 
 export PATH=$HOME/tool-inst/bin:$HOME/.local/bin:$HOME/tool-inst/share/asn1scc:$PATH
