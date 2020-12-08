8b7464ad76956b108bc20fcdd85423c5d10393c4
Thanassis Tsiodras <ttsiodras@gmail.com>
2020-12-08 16:10:43 +0100

    Trigger CI for ZynQ regression check.

---
+++ gitlabci-test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+/etc/init.d/postgresql start 
+source ~/.bashrc
+set -e
+export GIT_SSL_NO_VERIFY=true 
+export TASTE_IN_DOCKER=1 
+git fetch
+git checkout -f "${CI_COMMIT_BRANCH}" 
+./Update-TASTE.sh
+#echo y | ./add-ons/install-msp430-gcc.sh  || exit 1
+#./add-ons/install-gnat2020-for-arm.sh  || exit 1
+#echo y | ./add-ons/install-adac-hybrid-msp430.sh  || exit 1
+#echo y | ./add-ons/install-freertos.sh  || exit 1
+#source ~/.bashrc.taste
+cd kazoo 
+export PATH=$HOME/tool-inst/bin:$HOME/.local/bin:$HOME/tool-inst/share/asn1scc:$PATH
+export PATH=$HOME/tool-inst/share/kazoo:$PATH
+export PATH=/opt/rtems-5.1-2019.07.25/bin:$PATH
+export OCARINA_PATH=$HOME/tool-inst
+export LD_LIBRARY_PATH=$HOME/tool-inst/lib:$LD_LIBRARY_PATH
+make test || exit 1
+cd ..
+cd dmt
+git log | head
+./configure
+pip3 install --upgrade .
+pip3 uninstall --yes typing 
+PATH=$PATH:/asn1scc/ LANG=C LC_ALL=C make
+cd ..
+cd opengeode
+make test-ada || exit 1
