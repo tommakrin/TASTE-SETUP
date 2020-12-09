11e9f11f93c9235337fbaeb76a6129f780aa2501
Thanassis Tsiodras <ttsiodras@gmail.com>
2020-12-09 10:54:01 +0100

    Add dependency for GDB in Buster environments.

---
+++ install/92_leon3_simulator_support.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+[ -d /usr/lib/python3.5 ] || {
+    echo "[-] Installing support files from old Debian for Leon3 simulation..."
+    OLD_PY=/tmp/python3.5.tar.gz
+    if wget -O $OLD_PY "https://download.tuxfamily.org/taste/python3.5.tar.bz2" ; then
+        cd /usr/lib
+        sudo tar xpvf $OLD_PY || {
+            echo "[x] Failed to install $OLD_PY..."
+            ls -l $OLD_PY
+            echo "[x] Aborting."
+            exit 1
+        }
+        rm -f $OLD_PY
+    else
+        echo "[x] Failed to download the dependencies for the Leon3 simulator... Aborting."
+        exit 1
+    fi
+} && exit 0
