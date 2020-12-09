08004ce63d2c656c072e05ffb11555fe95830c89
Thanassis Tsiodras <ttsiodras@gmail.com>
2020-12-09 14:32:31 +0100

    Gitlab caches are broken! Undo debugging prints.

--- Update-TASTE.sh
+++ Update-TASTE.sh
@@ -17,12 +17,7 @@ function banner()
     echo -e "${COLOROFF}"
 }
 
-echo "[-] Before git pull"
-git branch -a
 git pull || exit 1
-echo "[-] After git pull"
-git branch -a
-exit 1
 if [ -z "$1" -o "$1" == "--stable" ] ; then
     git submodule init || exit 1
     git submodule update || exit 1
