f4954e7fb55e49fa774a7f8a076176ced3d99114
Thanassis Tsiodras <ttsiodras@gmail.com>
2020-12-09 14:43:35 +0100

    Don't re-checkout when doing Gitlab CI buids.

--- Update-TASTE.sh
+++ Update-TASTE.sh
@@ -17,18 +17,20 @@ function banner()
     echo -e "${COLOROFF}"
 }
 
-git pull || exit 1
-if [ -z "$1" -o "$1" == "--stable" ] ; then
-    git submodule init || exit 1
-    git submodule update || exit 1
-else
-    git submodule | awk '{print $2}' | while read FOLDER ; do
-        cd "$FOLDER" || exit 1
-        git fetch || exit 1
-        git checkout master || exit 1
-        git pull || exit 1
-        cd ..
-    done
+if [ -z "GITLAB_CI" ] ; then
+    git pull || exit 1
+    if [ -z "$1" -o "$1" == "--stable" ] ; then
+        git submodule init || exit 1
+        git submodule update || exit 1
+    else
+        git submodule | awk '{print $2}' | while read FOLDER ; do
+            cd "$FOLDER" || exit 1
+            git fetch || exit 1
+            git checkout master || exit 1
+            git pull || exit 1
+            cd ..
+        done
+    fi
 fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export DISABLE_TASTE_BANNER=1
