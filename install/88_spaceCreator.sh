b602270cc50bd21e6fd021e965f4c71d31e55bc3
Maxime Perrotin <Maxime.Perrotin@esa.int>
2021-02-19 09:57:31 +0100

    update space creator

--- install/88_spaceCreator.sh
+++ install/88_spaceCreator.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-dpkg -l | grep '^ii.*spacecreator.*0.1.34313' > /dev/null || {
+dpkg -l | grep '^ii.*spacecreator.*0.1.34409' > /dev/null || {
     dpkg -l | grep spacecreator > /dev/null && {
         echo "[-] Uninstalling previously existing version..."
         sudo apt remove -y --force-yes spacecreator || exit 1
     }
     echo "[-] Installing the latest Space Creator..."
     NEW_DEB=/tmp/newSpaceCreator.$$.deb
-    if wget -O $NEW_DEB "https://download.tuxfamily.org/taste/SpaceCreator-0.1.34313-Linux.deb" ; then
+    if wget -O $NEW_DEB "https://download.tuxfamily.org/taste/SpaceCreator-0.1.34409-Linux.deb" ; then
         sudo gdebi -n -o=--no-install-recommends $NEW_DEB || {
             echo "[x] Failed to install $NEW_DEB..."
             ls -l $NEW_DEB
@@ -26,3 +26,13 @@ mkdir -p ~/.local/share/QtProject/QtCreator/contextMenu || exit 1
 cp -u misc/space-creator/default_colors.json ~/.local/share/qtcreator/colors/default_colors.json  || exit 1
 cp -u misc/space-creator/contextmenu.json ~/.local/share/QtProject/QtCreator/contextMenu/ || exit 1
 cp -u misc/space-creator/default_attributes.xml ~/.local/share/QtProject/QtCreator/ || exit 1
+echo "Installing Syntax Highlighting files for Qt Creator"
+# The "fallback" folder of Qt Creator may be either Kate or Qt Creator's install folder, depending on
+# the machine setup. We put the files in both locations to be sure it works everywhere
+cp -u /usr/share/kde4/apps/katepart/syntax/ada.xml ~/.config/QtProject/qtcreator/generic-highlighter
+sudo cp -u /usr/share/kde4/apps/katepart/syntax/ada.xml /usr/share/qtcreator/generic-highlighter
+cp -u misc/space-creator/syntax/*  ~/.config/QtProject/qtcreator/generic-highlighter
+sudo cp -u misc/space-creator/syntax/* /usr/share/kde4/apps/katepart/syntax
+
+
+
