e9d4a49b9617f336d71e6b15716bc80e82472e99
Maxime Perrotin <Maxime.Perrotin@esa.int>
2021-02-23 17:51:14 +0100

    Update space creator

--- install/88_spaceCreator.sh
+++ install/88_spaceCreator.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-dpkg -l | grep '^ii.*spacecreator.*0.1.34409' > /dev/null || {
+dpkg -l | grep '^ii.*spacecreator.*0.1.34486' > /dev/null || {
     dpkg -l | grep spacecreator > /dev/null && {
         echo "[-] Uninstalling previously existing version..."
         sudo apt remove -y --force-yes spacecreator || exit 1
     }
     echo "[-] Installing the latest Space Creator..."
     NEW_DEB=/tmp/newSpaceCreator.$$.deb
-    if wget -O $NEW_DEB "https://download.tuxfamily.org/taste/SpaceCreator-0.1.34409-Linux.deb" ; then
+    if wget -O $NEW_DEB "https://download.tuxfamily.org/taste/SpaceCreator-0.1.34486-Linux.deb" ; then
         sudo gdebi -n -o=--no-install-recommends $NEW_DEB || {
             echo "[x] Failed to install $NEW_DEB..."
             ls -l $NEW_DEB
