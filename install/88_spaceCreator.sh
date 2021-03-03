0c141404d646cfb7533b67570ddf7554ce98262c
Maxime Perrotin <Maxime.Perrotin@esa.int>
2021-03-03 10:44:13 +0100

    Update space creator

--- install/88_spaceCreator.sh
+++ install/88_spaceCreator.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-dpkg -l | grep '^ii.*spacecreator.*0.1.34486' > /dev/null || {
+dpkg -l | grep '^ii.*spacecreator.*0.1.34763' > /dev/null || {
     dpkg -l | grep spacecreator > /dev/null && {
         echo "[-] Uninstalling previously existing version..."
         sudo apt remove -y --force-yes spacecreator || exit 1
     }
     echo "[-] Installing the latest Space Creator..."
     NEW_DEB=/tmp/newSpaceCreator.$$.deb
-    if wget -O $NEW_DEB "https://download.tuxfamily.org/taste/SpaceCreator-0.1.34486-Linux.deb" ; then
+    if wget -O $NEW_DEB "https://download.tuxfamily.org/taste/SpaceCreator-0.1.34763-Linux.deb" ; then
         sudo gdebi -n -o=--no-install-recommends $NEW_DEB || {
             echo "[x] Failed to install $NEW_DEB..."
             ls -l $NEW_DEB
@@ -31,7 +31,7 @@ cp -u misc/space-creator/default_attributes.xml ~/.local/share/QtProject/QtCreat
 # the machine setup. We put the files in both locations to be sure it works everywhere
 #cp   -f -u /usr/share/kde4/apps/katepart/syntax/ada.xml ~/.config/QtProject/qtcreator/generic-highlighter
 #sudo cp -f -u /usr/share/kde4/apps/katepart/syntax/ada.xml /usr/share/qtcreator/generic-highlighter
-cp -u misc/space-creator/syntax/*  ~/.config/QtProject/qtcreator/generic-highlighter
+cp -u misc/space-creator/syntax/*  ~/.config/QtProject/qtcreator/generic-highlighter || :
 #sudo cp -u misc/space-creator/syntax/* /usr/share/kde4/apps/katepart/syntax
 
 
