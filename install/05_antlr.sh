465dcbce3848d27b071aa7751648ae129fc8c508
Maxime Perrotin <Maxime.Perrotin@esa.int>
2021-03-23 17:28:01 +0100

    make sure the proper Python3/ANTLR3 runtime is installed
    the version from Debian repos, despite having the same version number (3.4) has
    an incompatible API, and this causes exceptions in Opengeode

--- install/05_antlr.sh
+++ install/05_antlr.sh
@@ -4,8 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Setup ANTL3 support in ~/.local/bin
 cd $DIR/../antlr/antlr3_python3_runtime_3.4 || exit 1
-pip3 freeze | grep antlr-python3-runtime >/dev/null \
-    || pip3 install --user --upgrade . || exit 1
+pip3 install --user --upgrade . || exit 1
 
 # Add .local/bin to PATH
 PATH_CMD='export PATH=$PATH:$HOME/.local/bin'
