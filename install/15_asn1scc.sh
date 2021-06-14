c2af88531d0c73ca61b705d09d48c4bcbb17d743
Thanassis Tsiodras <Thanassis.Tsiodras@esa.int>
2021-06-14 13:16:46 +0200

    Install the .NET Core runtime (2x speedier ASN1SCC!)

--- install/15_asn1scc.sh
+++ install/15_asn1scc.sh
@@ -2,6 +2,26 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/common.sh
 
+dpkg -l | grep '^ii.*packages-microsoft-prod.*1.0-debian10.1' > /dev/null || {
+    dpkg -l | grep packages-microsoft-prod > /dev/null && {
+        echo "[-] Uninstalling previously existing version..."
+        sudo apt remove -y --force-yes packages-microsoft-prod || exit 1
+    }
+    echo "[-] Installing the latest .NET run-time..."
+    NEW_DEB=/tmp/dotnetcore.$$.deb
+    if wget -O $NEW_DEB "https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb" ; then
+        sudo gdebi -n -o=--no-install-recommends $NEW_DEB || {
+            echo "[x] Failed to install $NEW_DEB..."
+            ls -l $NEW_DEB
+            echo "[x] Aborting."
+            exit 1
+        }
+        rm -f $NEW_DEB
+    else
+        echo "[x] Failed to download the latest .NET run-time... Aborting."
+        exit 1
+    fi
+}
 # Fetch and install latest ASN1SCC release
 mkdir -p "${PREFIX}/share/asn1scc/" || exit 1
 cd "${PREFIX}/share/" || exit 1
