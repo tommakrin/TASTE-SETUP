406856be8dff98f666ab516a582f5f58401d2758
Thanassis Tsiodras <Thanassis.Tsiodras@esa.int>
2021-06-18 10:01:21 +0200

    No more mono.

--- install/15_asn1scc.sh
+++ install/15_asn1scc.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Fetch and install latest ASN1SCC release
 mkdir -p "${PREFIX}/share/asn1scc/" || exit 1
 cd "${PREFIX}/share/" || exit 1
-VER=$(mono ~/tool-inst/share/asn1scc/asn1scc -v |  head -1 | awk '{print $NF}')
+VER=$(~/tool-inst/share/asn1scc/asn1scc -v |  head -1 | awk '{print $NF}')
 if [ "${VER}" != "4.2.4.7f" ] ; then
     wget -q -O - https://github.com/ttsiodras/asn1scc/releases/download/4.2.4.7f/asn1scc-bin-4.2.4.7f.tar.bz2 \
         | tar jxvf -
