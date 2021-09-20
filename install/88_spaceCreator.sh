2e09a7f3fb7d4b7df87bbebb44014f1c8a92e08d
Maxime Perrotin <maxime.perrotin@esa.int>
2021-09-20 07:18:25 -0400

    Update space creator

--- install/88_spaceCreator.sh
+++ install/88_spaceCreator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Version neeed by the current TASTE release.
 # Update this number when a new version is uploaded on tuxfamily:
-EXPECTED_VERSION="0.10.5055"
+EXPECTED_VERSION="0.10.5090"
 FILENAME=spacecreator-x86_64-$EXPECTED_VERSION.AppImage
 
 # Check the version of the current insallation, if any
