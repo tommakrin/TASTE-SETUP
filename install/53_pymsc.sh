3a8db971e61318d2876fb125cd37a4f566c7aac8
Maxime Perrotin <maxime.perrotin@esa.int>
2021-10-07 09:50:22 -0400

    Replace pip3 with python3 -m pip

--- install/53_pymsc.sh
+++ install/53_pymsc.sh
@@ -8,7 +8,7 @@ cd "$DIR/../pymsc" || exit 1
 # Skip install if the version installed is the same and the tree is clean
 HEAD="$(grep __version python3/msccore/__init__.py | awk '{print $NF}' | tr -d '"')"
 
-VERSION_INSTALLED="$(pip3 freeze | grep taste-msc | awk -F= '{print $NF}')"
+VERSION_INSTALLED="$(python3 -m pip freeze | grep taste-msc | awk -F= '{print $NF}')"
 GIT_OUTPUT=$(git status --porcelain)
 if [ "${GIT_OUTPUT}" == "" ] ; then
     TREE_DIRTY=0
@@ -22,7 +22,7 @@ fi
 
 # Unfortunately, the --upgrade DOES NOT ALWAYS WORK.
 # Uninstall first...
-echo y | pip3 uninstall taste-msc
+echo y | python3 -m pip uninstall taste-msc
 echo y | pip2 uninstall taste-msc
 make install || exit 1
 
