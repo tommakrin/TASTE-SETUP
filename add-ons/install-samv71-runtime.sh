20fffb4efa65a9c0b212bed187bb5720c6d5b150
Filip Demski <fdemski@n7space.com>
2021-11-05 16:21:00 +0100

    SAMV71: Created installation script for the runtime

---
+++ add-ons/install-samv71-runtime.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. "${DIR}"/common.sh
+
+#
+# TASTE-SAMV71-Runtime
+#
+
+echo "Installing TASTE-SAMV71-Runtime"
+
+cd "$DIR"/../TASTE-SAMV71-Runtime || exit 1
+
+git submodule init
+git submodule update
+
+mkdir -p "${PREFIX}"/include/TASTE-SAMV71-Runtime
+# delete old files
+rm -rf "${PREFIX}"/include/TASTE-SAMV71-Runtime/*
+# install
+cp -r FreeRTOS-Kernel "${PREFIX}"/include/TASTE-SAMV71-Runtime/
+cp -r SAMV71-BSP "${PREFIX}"/include/TASTE-SAMV71-Runtime/
