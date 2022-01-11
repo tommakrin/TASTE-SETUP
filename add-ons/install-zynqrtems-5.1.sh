a20cb7d3d5c5f584eb4f47d0928457e1c96aebe7
Thanassis Tsiodras <Thanassis.Tsiodras@esa.int>
2022-01-11 16:09:04 +0100

    Addon to install ZynQ RTEMS cross-compiler

---
+++ add-ons/install-zynqrtems-5.1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# shellcheck source=common.sh
+. "${DIR}/common.sh"
+
+DESCRIPTION="ZynQ RTEMS 5.1"
+PREFIX="/opt"
+INSTALL_PATH="$PREFIX/rtems-5.1-2020.04.29"
+URL="https://download.tuxfamily.org/taste/rtems-5.1-2020.04.29.tar.bz2"
+
+CheckTargetFolder "${DESCRIPTION}" "${INSTALL_PATH}"
+DownloadAndExtract "${DESCRIPTION}" "${URL}" "${PREFIX}"
